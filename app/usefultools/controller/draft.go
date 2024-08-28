7fa367ad4071bfcdb7f338929d2f99f7f2ec91e4
pengjunwen <2397711850@qq.com>
2024-08-28 21:05:02 +0800

    feat: 完成草稿纸页面

--- app/usefultools/controller/draft.go
+++ app/usefultools/controller/draft.go
@@ -1,39 +1,85 @@
 package controller
 
-import "useful-tools/app/usefultools/model"
+import (
+	"github.com/sagernet/sing/common/x/linkedhashmap"
+	"useful-tools/app/usefultools/model"
+)
 
 type Draft struct {
-	leftDocs  map[string]model.Doc
-	rightDocs map[string]model.Doc
+	leftDocs  linkedhashmap.Map[string, model.Doc]
+	rightDocs linkedhashmap.Map[string, model.Doc]
 }
 
 func NewDraft() *Draft {
-	return &Draft{
-		leftDocs:  make(map[string]model.Doc),
-		rightDocs: make(map[string]model.Doc),
-	}
+	return &Draft{}
 }
 
-func (d *Draft) LeftDocs() map[string]model.Doc {
-	return d.leftDocs
+func (d *Draft) LeftDocs() []model.Doc {
+	return d.leftDocs.Values()
 }
 
-func (d *Draft) RightDocs() map[string]model.Doc {
-	return d.rightDocs
+func (d *Draft) RightDocs() []model.Doc {
+	return d.rightDocs.Values()
 }
 
 func (d *Draft) AddLeftDocs(title, content, placeHolder string) {
-	d.leftDocs[title] = model.Doc{
+	d.leftDocs.Put(title, model.Doc{
 		Title:       title,
 		Content:     content,
 		PlaceHolder: placeHolder,
-	}
+	})
 }
 
 func (d *Draft) AddRightDocs(title, content, placeHolder string) {
-	d.rightDocs[title] = model.Doc{
+	d.rightDocs.Put(title, model.Doc{
 		Title:       title,
 		Content:     content,
 		PlaceHolder: placeHolder,
+	})
+}
+
+func (d *Draft) FindRightNextDocsIndex(title string) (model.Doc, int) {
+	for i, doc := range d.rightDocs.Values() {
+		if title == doc.Title {
+			a := i + 1
+			if a >= d.rightDocs.Size() {
+				return model.Doc{}, -1
+			}
+			return doc, a
+		}
 	}
+	return model.Doc{}, -1
+}
+
+func (d *Draft) FindRightPrevDocsIndex(title string) (model.Doc, int) {
+	for i, doc := range d.rightDocs.Values() {
+		if title == doc.Title {
+			a := i - 1
+			return doc, a
+		}
+	}
+	return model.Doc{}, -1
+}
+
+func (d *Draft) FindLeftNextDocsIndex(title string) (model.Doc, int) {
+	for i, doc := range d.leftDocs.Values() {
+		if title == doc.Title {
+			a := i + 1
+			if a >= d.leftDocs.Size() {
+				return model.Doc{}, -1
+			}
+			return doc, a
+		}
+	}
+	return model.Doc{}, -1
+}
+
+func (d *Draft) FindLeftPrevDocsIndex(title string) (model.Doc, int) {
+	for i, doc := range d.leftDocs.Values() {
+		if title == doc.Title {
+			a := i - 1
+			return doc, a
+		}
+	}
+	return model.Doc{}, -1
 }
--- app/usefultools/view/draft/draft.go
+++ app/usefultools/view/draft/draft.go
@@ -12,13 +12,15 @@ import (
 )
 
 var (
-	logics = controller.NewDraft()
-	view   *widget.Entry
-	scroll *container.Scroll
+	logics    = controller.NewDraft()
+	view      *widget.Entry
+	scroll    *container.Scroll
+	rightTabs *container.DocTabs
+	leftTabs  *container.DocTabs
 )
 
 func Screen(w fyne.Window, mode constant.ViewMode) fyne.CanvasObject {
-	return container.NewHSplit(leftScreen(mode), rightScreen(w))
+	return container.NewHSplit(leftScreen(mode), rightScreen(mode))
 }
 
 func leftScreen(mode constant.ViewMode) fyne.CanvasObject {
@@ -27,79 +29,128 @@ func leftScreen(mode constant.ViewMode) fyne.CanvasObject {
 	top := viewWidget.MakeCellSize(0, 0)
 	bottom := viewWidget.MakeCellSize(10, 10)
 
+	var initTabItems []*container.TabItem
+
 	var docs []*container.TabItem
 	if len(logics.LeftDocs()) > 0 {
-		for t, s := range logics.LeftDocs() {
-			docs = append(docs, createLeftCanvasObject(t, s.PlaceHolder, s.Content))
+		for _, s := range logics.LeftDocs() {
+			docs = append(docs, createLeftCanvasObject(s.Title, s.PlaceHolder, s.Content))
 		}
 	} else {
-		docs = append(docs, createLeftCanvasObject("草稿1", "草稿1", ""), createLeftCanvasObject("草稿2", "草稿2", ""), createLeftCanvasObject("草稿3", "草稿3", ""))
+		initTabItems = []*container.TabItem{
+			createLeftCanvasObject("草稿1", "草稿1", ""),
+			createLeftCanvasObject("草稿2", "草稿2", ""),
+			createLeftCanvasObject("草稿3", "草稿3", ""),
+		}
+		docs = append(docs, initTabItems...)
 	}
 
-	tabs := container.NewDocTabs(
+	leftTabs = container.NewDocTabs(
 		docs...,
 	)
-	i := 3
-	tabs.CreateTab = func() *container.TabItem {
+	i := len(initTabItems)
+	leftTabs.CreateTab = func() *container.TabItem {
 		i++
 		return createLeftCanvasObject(fmt.Sprintf("草稿%d", i), fmt.Sprintf("草稿%d", i), "")
 	}
-	tabs.OnClosed = func(item *container.TabItem) {
-		logrus.Infof("closed tab: %s", item.Text)
+	leftTabs.OnSelected = func(item *container.TabItem) {
+		logrus.Infof("selected tab: %s", item.Text)
 	}
-	return container.NewBorder(top, bottom, left, right, tabs)
+	return container.NewBorder(top, bottom, left, right, leftTabs)
 }
 
 func createLeftCanvasObject(title, placeHolder, text string) *container.TabItem {
-	entry := widget.NewMultiLineEntry()
-	entry.PlaceHolder = placeHolder
+	leftMultiEntry := widget.NewMultiLineEntryEx(func() {
+		if _, idx := logics.FindLeftNextDocsIndex(leftTabs.Selected().Text); idx >= 0 {
+			leftTabs.SelectIndex(idx)
+		} else {
+			logrus.Warnf("can't find prev doc: %s", leftTabs.Selected().Text)
+		}
+	}, func() {
+		if _, idx := logics.FindLeftPrevDocsIndex(leftTabs.Selected().Text); idx >= 0 {
+			leftTabs.SelectIndex(idx)
+		} else {
+			logrus.Warnf("can't find next doc: %s", leftTabs.Selected().Text)
+		}
+	}, func() {
+		leftTabs.Append(leftTabs.CreateTab())
+		logrus.Infof("create new left doc: %s", leftTabs.Selected().Text)
+		leftTabs.SelectIndex(len(leftTabs.Items) - 1)
+	})
+	leftMultiEntry.PlaceHolder = placeHolder
 	if text != "" {
-		entry.SetText(text)
+		leftMultiEntry.SetText(text)
 	}
-	entry.OnChanged = func(s string) {
+	leftMultiEntry.OnChanged = func(s string) {
 		logrus.Infof("left draft doc[%s] content: %s", title, s)
 		logics.AddLeftDocs(title, s, placeHolder)
 	}
 	logics.AddLeftDocs(title, text, placeHolder)
-	return container.NewTabItem(title, entry)
+	return container.NewTabItem(title, leftMultiEntry)
 }
 
 func createRightCanvasObject(title, placeHolder, text string) *container.TabItem {
-	entry := widget.NewMultiLineEntry()
-	entry.PlaceHolder = placeHolder
+	rightMultiEntry := widget.NewMultiLineEntryEx(func() {
+		if _, idx := logics.FindRightNextDocsIndex(rightTabs.Selected().Text); idx >= 0 {
+			rightTabs.SelectIndex(idx)
+		} else {
+			logrus.Warnf("can't find prev doc: %s", rightTabs.Selected().Text)
+		}
+	}, func() {
+		if _, idx := logics.FindRightPrevDocsIndex(rightTabs.Selected().Text); idx >= 0 {
+			rightTabs.SelectIndex(idx)
+		} else {
+			logrus.Warnf("can't find next doc: %s", rightTabs.Selected().Text)
+		}
+	}, func() {
+		rightTabs.Append(rightTabs.CreateTab())
+		logrus.Infof("create new right doc: %s", rightTabs.Selected().Text)
+		rightTabs.SelectIndex(len(rightTabs.Items) - 1)
+	})
+	rightMultiEntry.PlaceHolder = placeHolder
 	if text != "" {
-		entry.SetText(text)
+		rightMultiEntry.SetText(text)
 	}
-	entry.OnChanged = func(s string) {
+	rightMultiEntry.OnChanged = func(s string) {
 		logrus.Infof("right draft doc[%s] content: %s", title, s)
-		logics.AddLeftDocs(title, s, placeHolder)
+		logics.AddRightDocs(title, s, placeHolder)
 	}
 	logics.AddRightDocs(title, text, placeHolder)
-	return container.NewTabItem(title, entry)
+	return container.NewTabItem(title, rightMultiEntry)
 }
 
-func rightScreen(w fyne.Window) fyne.CanvasObject {
+func rightScreen(mode constant.ViewMode) fyne.CanvasObject {
 	right := viewWidget.MakeCellSize(10, 10)
 	left := viewWidget.MakeCellSize(10, 10)
 	top := viewWidget.MakeCellSize(0, 0)
 	bottom := viewWidget.MakeCellSize(10, 10)
 
+	var initTabItems []*container.TabItem
+
 	var docs []*container.TabItem
 	if len(logics.RightDocs()) > 0 {
-		for t, s := range logics.RightDocs() {
-			docs = append(docs, createRightCanvasObject(t, s.PlaceHolder, s.Content))
+		for _, s := range logics.RightDocs() {
+			docs = append(docs, createRightCanvasObject(s.Title, s.PlaceHolder, s.Content))
 		}
 	} else {
-		docs = append(docs, createRightCanvasObject("草稿1", "草稿1", ""), createRightCanvasObject("草稿2", "草稿2", ""), createRightCanvasObject("草稿3", "草稿3", ""))
+		initTabItems = []*container.TabItem{
+			createRightCanvasObject("草稿1", "草稿1", ""),
+			createRightCanvasObject("草稿2", "草稿2", ""),
+			createRightCanvasObject("草稿3", "草稿3", ""),
+		}
+		docs = append(docs, initTabItems...)
 	}
 
-	tabs := container.NewDocTabs(
+	rightTabs = container.NewDocTabs(
 		docs...,
 	)
-	i := 3
-	tabs.CreateTab = func() *container.TabItem {
+	i := len(initTabItems)
+	rightTabs.CreateTab = func() *container.TabItem {
 		i++
 		return createRightCanvasObject(fmt.Sprintf("草稿%d", i), fmt.Sprintf("草稿%d", i), "")
 	}
-	return container.NewBorder(top, bottom, left, right, tabs)
+	rightTabs.OnSelected = func(item *container.TabItem) {
+		logrus.Infof("selected tab: %s", item.Text)
+	}
+	return container.NewBorder(top, bottom, left, right, rightTabs)
 }
