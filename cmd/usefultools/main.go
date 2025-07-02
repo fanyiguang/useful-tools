d3931d734cc45d146d24e0986eec189c3a3042e9
fanyiguan <2397711850@qq.com>
2025-07-02 17:01:31 +0800

    test

--- cmd/usefultools/main.go
+++ cmd/usefultools/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
-	"github.com/sirupsen/logrus"
 	"useful-tools/app/usefultools"
+
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
+	logrus.SetLevel(logrus.DebugLevel)
 	err := usefultools.App()
 	if err != nil {
 		logrus.Errorf("%+v", err)
